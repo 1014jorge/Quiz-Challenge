1d22b84bf192bf710b23121642c5ba1ff9990e6b
Jorge Gonzalez <jorge.1014@gmail.com>
2023-07-27 08:36:41 -0700

    commit1

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Javacript is a(n)_______language",
+        choices: ["Procedural", "Object-Oriented", "Foreign", "None of the Above"],
+        answer: "Object-Oriented"
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["var", "varmit", "parsecs", "change"],
+        answer: "var"
+    },
+    {
+        title: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choices: ["parse()", "stringify()", "convert()", "Vorhees"],
+        answer: "stringify"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "Which of the following are closures in Javascript?",
+        choices: ["variables", "functions", "objects", "All of the Above"],
+        answer: "All of the Above"
+    }
+]
